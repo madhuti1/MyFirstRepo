ef8671d712f1e0b8778a1eb6e44c76028851222a
madhuti1 <ti.madhukar@gmail.com>
2019-10-29 01:27:38 +0000

    modified the code

--- mycode.c
+++ mycode.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 
-void main(void) {
-  int i=0;
-  for (; i < 10; i++) 
-    printf("Hello Code");
-return;
+int my_strlen(char *s)
+{
+   char *p = s;
+
+   while (*p)
+      ++p;
+
+   return (p - s);
 }
 
+int main(void)
+{
+   int i;
+   char *s[] = 
+   {
+      "Git tutorials",
+      "Tutorials Point"
+   };
+
+   for (i = 0; i < 2; ++i)
+      
+   printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+
+   return 0;
+}

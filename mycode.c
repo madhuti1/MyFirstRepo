688dda1393f2a72e8178f419cbdc1b1570632063
madhuti1 <ti.madhukar@gmail.com>
2019-10-29 01:44:39 +0000

    Removed spaces println

--- mycode.c
+++ mycode.c
@@ -3,10 +3,8 @@
 int my_strlen(char *s)
 {
    char *p = s;
-
    while (*p)
       ++p;
-
    return (p - s);
 }
 
@@ -15,8 +13,8 @@ int main(void)
    int i;
    char *s[] = 
    {
-      "Git tutorials",
-      "Tutorials Point"
+      "G tutorials",
+      "Tials Point"
    };
 
    for (i = 0; i < 2; ++i)
